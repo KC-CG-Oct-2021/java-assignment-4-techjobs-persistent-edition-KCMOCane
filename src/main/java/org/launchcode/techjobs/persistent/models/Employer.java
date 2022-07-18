5aa664021cde7f16a8f4eb80ac930c398bbb9fa6
KCMOCane <kcmocane@gmail.com>
2022-07-18 14:33:01 -0500

    fixing errors continued

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -8,13 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
-    @Size(max = 255)
+    @Size(min = 4, max = 100, message = "Location must be between 4 and 100 characters")
+    @NotNull
     private String location;
 
     @OneToMany
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -15,6 +15,11 @@ public class Skill extends AbstractEntity {
     @ManyToMany(mappedBy = "skills")
     private List<Job> jobs = new ArrayList<>();
 
+    public Skill(String description, List<Job> jobs) {
+        this.description = description;
+        this.jobs = jobs;
+    }
+
     public Skill(){}
 
     public String getDescription() {
@@ -23,5 +28,13 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
